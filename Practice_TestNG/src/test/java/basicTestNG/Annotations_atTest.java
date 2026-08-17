d94456feb86a4d000b0e71d5e8eedf912be6aacf
Anusha Hiremath <anusha.hiremath.qa@gmail.com>
2026-08-17 21:30:05 +0200

    Annotations: atTest

--- Practice_TestNG/src/test/java/basicTestNG/Annotations_atTest.java
+++ Practice_TestNG/src/test/java/basicTestNG/Annotations_atTest.java
@@ -1,24 +1,48 @@
 package basicTestNG;
 
+
+
 import org.testng.annotations.Test;
 
+
+
 public class Annotations_atTest {
-	
+
+
+
 	@Test(priority = 0)
+
 	public void login() {
+
 		System.out.println("Login Script");
+
 	}
 
+
+
 	@Test(priority = 1)
+
 	public void inbox() {
+
 		System.out.println("Inbox Script");
+
 	}
-	
+
+
+
 	@Test(priority = 2)
-	
+
+
+
 	public void compose() {
+
 		System.out.println("Compose Script");
+
 	}
-	
-	
+
+
+
+
+
 }
+
