e0c99370575d214dda16d7e46326b4ea815f6a32
Anusha Hiremath <anusha.hiremath.qa@gmail.com>
2026-08-18 21:08:09 +0200

    Annotations: atBeforeMETHOD and atafterMETHOD

--- Practice_TestNG/src/test/java/basicTestNG/Annotations_atBeforeAfterMETHOD.java
+++ Practice_TestNG/src/test/java/basicTestNG/Annotations_atBeforeAfterMETHOD.java
@@ -1,45 +1,47 @@
 package basicTestNG;
 
-
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class Annotations_atBeforeAfter {
+public class Annotations_atBeforeAfterMETHOD {
 
 	@Test(priority = 0)
-
 	public void login() {
-
 		System.out.println("Login Script");
-
 	}
 
 	@Test(priority = 1)
-
 	public void inbox() {
-
 		System.out.println("Inbox Script");
-
 	}
 
 	@Test(priority = 2)
-
 	public void compose() {
-
 		System.out.println("Compose Script");
-
 	}
-	
+
 	@BeforeTest
 	public void openbrowser() {
 		System.out.println("Open browser script");
 	}
-	
+
 	@AfterTest
 	public void closebrowser() {
 		System.out.println("Close broswer script");
 	}
-	
+
+	@BeforeMethod // executed before each test cases
+	public void beforemethod() {
+		System.out.println("Before method");
+
+	}
+	@AfterMethod // executed after each test cases
+	public void aftermethod() {
+		System.out.println("After method");
+
+	}
 
 }
---
+++ Practice_TestNG/src/test/java/basicTestNG/Annotations_atBeforeAfterTEST.java
@@ -0,0 +1,45 @@
+package basicTestNG;
+
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Annotations_atBeforeAfterTEST {
+
+	@Test(priority = 0)
+
+	public void login() {
+
+		System.out.println("Login Script");
+
+	}
+
+	@Test(priority = 1)
+
+	public void inbox() {
+
+		System.out.println("Inbox Script");
+
+	}
+
+	@Test(priority = 2)
+
+	public void compose() {
+
+		System.out.println("Compose Script");
+
+	}
+	
+	@BeforeTest
+	public void openbrowser() {
+		System.out.println("Open browser script");
+	}
+	
+	@AfterTest
+	public void closebrowser() {
+		System.out.println("Close broswer script");
+	}
+	
+
+}
