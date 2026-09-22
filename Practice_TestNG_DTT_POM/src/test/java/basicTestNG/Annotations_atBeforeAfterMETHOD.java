d7101483c73951d307ae15b8f6c2b42c9dca50f2
Anusha Hiremath <anusha.hiremath.qa@gmail.com>
2026-09-22 12:18:43 +0200

    changed repository

--- Practice_TestNG_DTT_POM/src/test/java/basicTestNG/Annotations_atBeforeAfterMETHOD.java
+++ Practice_TestNG_DTT_POM/src/test/java/basicTestNG/Annotations_atBeforeAfterMETHOD.java
@@ -1,10 +1,11 @@
 package basicTestNG;
 
+import org.testng.annotations.Test;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+
 
 public class Annotations_atBeforeAfterMETHOD {
 
